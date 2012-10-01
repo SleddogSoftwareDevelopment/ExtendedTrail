f3f1af3fbb1407d1adb81f3807398800e2c78029
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-10-01 18:11:32 +0200

    Added loads of P/Invoke stuff

---
+++ src/Sleddog.ExtendedTrail/advApi32.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sleddog.ExtendedTrail
+{
+	public class advApi32
+	{
+		[DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+		public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+
+		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		private static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		public static extern IntPtr LockServiceDatabase(IntPtr hSCManager);
+
+		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool ChangeServiceConfig2(IntPtr hService, int dwInfoLevel, IntPtr lpInfo);
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool CloseServiceHandle(IntPtr hSCObject);
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		public static extern bool UnlockServiceDatabase(IntPtr hSCManager);
+	}
+}
