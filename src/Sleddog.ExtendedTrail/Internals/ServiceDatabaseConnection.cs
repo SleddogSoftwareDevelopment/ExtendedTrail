119884180a0c664b96317f9c83d2758435dcd286
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-11-07 00:33:59 +0100

    Fixed a messup with class and file alignment

--- src/Sleddog.ExtendedTrail/Internals/ServiceDatabaseConnection.cs
+++ src/Sleddog.ExtendedTrail/Internals/ServiceDatabaseConnection.cs
@@ -18,12 +18,4 @@ namespace Sleddog.ExtendedTrail.Internals
 			}
 		}
 	}
-
-	public class ServiceDatabaseConnectionException:Exception
-	{
-		public ServiceDatabaseConnectionException(string message):base(message)
-		{
-			
-		}	
-	}
 }
