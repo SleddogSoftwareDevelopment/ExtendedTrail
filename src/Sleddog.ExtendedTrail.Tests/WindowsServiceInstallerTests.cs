05037564076cb98e44bc0bf953e6a0d676a293f2
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-11-06 23:45:49 +0100

    Added test project

---
+++ src/Sleddog.ExtendedTrail.Tests/WindowsServiceInstallerTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Sleddog.ExtendedTrail.Tests
+{
+	public class WindowsServiceInstallerTests
+	{
+		[Fact]
+		public void CanInstantiate()
+		{
+			Assert.DoesNotThrow(() => new WindowsServiceInstaller());
+		}
+
+		[Fact]
+		public void FirstFailureOptionAreNotNullOnInitialize()
+		{
+			var sut = new WindowsServiceInstaller();
+
+			Assert.NotNull(sut.FirstFailure);
+		}
+
+		[Fact]
+		public void SecondFailureOptionAreNotNullOnInitialize()
+		{
+			var sut = new WindowsServiceInstaller();
+
+			Assert.NotNull(sut.SecondFailure);
+		}
+
+		[Fact]
+		public void SubsequentFailureOptionAreNotNullOnInitialize()
+		{
+			var sut = new WindowsServiceInstaller();
+
+			Assert.NotNull(sut.SubsequentFailure);
+		}
+	}
+}
