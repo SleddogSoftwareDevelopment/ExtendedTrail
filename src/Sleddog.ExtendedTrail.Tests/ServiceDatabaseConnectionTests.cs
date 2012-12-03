6e54f675bdb1b6e724d5f7d29952e4f027ec22d0
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-12-03 21:07:11 +0100

    Cleared some poorly written tests

--- src/Sleddog.ExtendedTrail.Tests/ServiceDatabaseConnectionTests.cs
+++ src/Sleddog.ExtendedTrail.Tests/ServiceDatabaseConnectionTests.cs
@@ -10,34 +10,41 @@ namespace Sleddog.ExtendedTrail.Tests
 	public class ServiceDatabaseConnectionTests
 	{
 		[Theory, AutoFake]
-		public void CanOpenServiceManager(Fake<IAdvApi32> advApi32, long pointerValue)
+		public void OpenServiceManagerGetsCorrectConnectionHandle(Fake<IAdvApi32> advApi32, long pointerValue)
 		{
 			advApi32.CallsTo(_ => _.OpenServiceControlManager(A<string>._, A<string>._, A<ScmAccess>._))
-				.Returns(new IntPtr(pointerValue));
+			        .Returns(new IntPtr(pointerValue));
 
 			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
 
 			var connectionHandle = sut.Open();
 
-			Assert.True(connectionHandle.ServiceManagerIsOpen);
+			var actual = connectionHandle.ServiceManagerHandle;
+			var expected = new IntPtr(pointerValue);
 
-			advApi32.CallsTo(_ => _.OpenServiceControlManager(A<string>._, A<string>._, A<ScmAccess>._)).MustHaveHappened();
+			Assert.Equal(expected, actual);
 		}
 
 		[Theory, AutoFake]
-		public void CanCloseServiceManager(Fake<IAdvApi32> advApi32, int serviceManagerHandleValue)
+		public void CloseServiceManagerResetsConnectionHandleToIntPtrZero(Fake<IAdvApi32> advApi32, int serviceManagerHandleValue)
 		{
+			advApi32.CallsTo(_ => _.CloseServiceControlManager(A<IntPtr>._))
+			        .Returns(true);
+
 			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
 
 			var connectionHandle = new ConnectionHandle {ServiceManagerHandle = new IntPtr(serviceManagerHandleValue)};
 
 			sut.Close(connectionHandle);
 
-			advApi32.CallsTo(_ => _.CloseServiceControlManager(A<IntPtr>._)).MustHaveHappened();
+			var actual = connectionHandle.ServiceManagerHandle;
+			var expected = IntPtr.Zero;
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Theory, AutoFake]
-		public void CanNotAquireWriteLockWithoutAnOpenConnection(Fake<IAdvApi32> advApi32)
+		public void AquireWriteLockRequiresConnectionHandle(Fake<IAdvApi32> advApi32)
 		{
 			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
 
@@ -45,10 +52,30 @@ namespace Sleddog.ExtendedTrail.Tests
 		}
 
 		[Theory, AutoFake]
-		public void CanAquireWriteLock(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue, int serviceDatabaseLockHandleValue)
+		public void WriteLockSetsServiceDatabaseLockHandle(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue,
+		                                                   int serviceDatabaseLockHandleValue)
 		{
 			advApi32.CallsTo(_ => _.AquireServiceDatabaseLock(A<IntPtr>._))
-				.Returns(new IntPtr(serviceDatabaseLockHandleValue));
+			        .Returns(new IntPtr(serviceDatabaseLockHandleValue));
+
+			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
+
+			var connectionHandle = new ConnectionHandle {ServiceManagerHandle = new IntPtr(serviceControlManagerHandlerValue)};
+
+			sut.WriteLock(connectionHandle);
+
+			var actual = connectionHandle.ServiceDatabaseLockHandle;
+			var expected = new IntPtr(serviceDatabaseLockHandleValue);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory, AutoFake]
+		public void WriteLockCallsUnderlyingAPI(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue,
+		                                        int serviceDatabaseLockHandleValue)
+		{
+			advApi32.CallsTo(_ => _.AquireServiceDatabaseLock(A<IntPtr>._))
+			        .Returns(new IntPtr(serviceDatabaseLockHandleValue));
 
 			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
 
@@ -60,7 +87,31 @@ namespace Sleddog.ExtendedTrail.Tests
 		}
 
 		[Theory, AutoFake]
-		public void CanReleaseWriteLock(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue, int serviceDatabaseLockHandleValue)
+		public void ReleaseWriteLockResetsDatabaseLockHandle(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue,
+		                                                     int serviceDatabaseLockHandleValue)
+		{
+			advApi32.CallsTo(_ => _.ReleaseServiceDatabaseLock(A<IntPtr>._))
+			        .Returns(true);
+
+			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
+
+			var connectionHandle = new ConnectionHandle
+				{
+					ServiceManagerHandle = new IntPtr(serviceControlManagerHandlerValue),
+					ServiceDatabaseLockHandle = new IntPtr(serviceDatabaseLockHandleValue)
+				};
+
+			sut.ReleaseLock(connectionHandle);
+
+			var actual = connectionHandle.ServiceDatabaseLockHandle;
+			var expected = IntPtr.Zero;
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory, AutoFake]
+		public void ReleaseWriteLockCallsUnderlyingAPI(Fake<IAdvApi32> advApi32, int serviceControlManagerHandlerValue,
+		                                               int serviceDatabaseLockHandleValue)
 		{
 			var sut = new ServiceDatabaseConnection(advApi32.FakedObject);
 
